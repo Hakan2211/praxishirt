65c39b5726c736c01b110a0dacc18331f449d9eb
hakan <hbilgic1992@gmail.com>
2023-10-15 16:10:45 +0200

    change intro text content

--- src/app/page.js
+++ src/app/page.js
@@ -7,16 +7,13 @@ export default function Home() {
     <>
       <main className="bg-gradient-to-t from-slate-100 via-blue-100 to-sky-200 h-screen">
         {/* jumbo */}
-        <div className="mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5">
-          <div className="flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left">
-            <p className="w-full uppercase">Next + React Three Fiber</p>
-            <h1 className="my-4 text-5xl font-bold leading-tight">
-              Next 3D Starter
-            </h1>
-            <p className="mb-8 text-2xl leading-normal">
-              A minimalist starter for React, React-three-fiber and Threejs.
-            </p>
-          </div>
+
+        <div className="flex w-full flex-col items-center justify-center p-12 text-center translate-y-full translate-x-60  ">
+          <span className="w-full text-2xl">Willkommen bei</span>
+          <h1 className="my-4 text-5xl font-bold leading-tight">Praxis Hirt</h1>
+          <p className="mb-8 text-2xl leading-normal">
+            Wo Fachkompetenz und Fürsorge eine Brücke schlagen
+          </p>
         </div>
 
         <Models />
