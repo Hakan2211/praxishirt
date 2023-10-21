642f8a2b2e17f54e4f146227e7d749b2c235bbb3
hakan <hbilgic1992@gmail.com>
2023-10-21 14:26:33 +0200

    add models with particles -brain,heart and dna

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -2,15 +2,29 @@
 
 import { View } from '@/components/canvas/View';
 import { useSoundEnabled } from '@/components/context/SoundEnabledProvider';
+import Brain from '@/components/homepage/brain/Brain';
+import BrainParticles from '@/components/homepage/brain/Particles';
+import DNA from '@/components/homepage/dna/DNA';
+import DNAParticles from '@/components/homepage/dna/DNAParticles';
+import Heart from '@/components/homepage/heart/Heart';
+import HeartParticles from '@/components/homepage/heart/HeartParticles';
 import { PositionalAudio } from '@react-three/drei';
+import dynamic from 'next/dynamic';
 import React from 'react';
+
+const Common = dynamic(
+  () => import('@/components/canvas/View').then((mod) => mod.Common),
+  { ssr: false }
+);
 
 function Homepage() {
   const { soundEnabled } = useSoundEnabled();
   return (
     <>
-      <div>Homepage</div>
-      <View>
+      <div className="">
+        <h1 className="text-white">Homepage</h1>
+      </div>
+      <View className=" absolute top-0 flex h-screen w-full flex-col items-center justify-center">
         {soundEnabled && (
           <PositionalAudio
             distance={2}
@@ -19,6 +33,13 @@ function Homepage() {
             url="../../../audio_background/Flight of the Inner Bird (Slowed Reverb).mp3"
           />
         )}
+        <Common color={'black'} />
+        {/* <Brain className="cursor-pointer" />
+        <BrainParticles /> */}
+        {/* <Heart scale={0.5} />
+        <HeartParticles /> */}
+        <DNA />
+        <DNAParticles />
       </View>
     </>
   );
