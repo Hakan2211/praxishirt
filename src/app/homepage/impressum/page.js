adaf9d5c118895d8e5bd9e734d1dc65ff434c7e5
hakan <hbilgic1992@gmail.com>
2026-03-01 11:47:03 +0100

    remove number from impressum

--- src/app/homepage/impressum/page.js
+++ src/app/homepage/impressum/page.js
@@ -103,7 +103,7 @@ function Impressum() {
                     (www.lpk-bw.de)
                   </a>
                 </p>
-                <p>Arztregister-Nummer: 5515798</p>
+                {/* <p>Arztregister-Nummer: 5515798</p> */}
               </div>
               <p className="mt-20">
                 * Die Titel „Psychologische Psychotherapeutin&quot; und
